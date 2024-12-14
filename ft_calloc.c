a710c8cc519e937d8a8f25bf9fb46fa6f5a968de
Amin <aminmaatallah4@gmail.com>
2024-12-14 16:06:45 +0100

    done v2

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,21 @@
 /*   By: amaatall <amaatall@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:44:51 by amaatall          #+#    #+#             */
-/*   Updated: 2024/12/11 23:40:07 by amaatall         ###   ########.fr       */
+/*   Updated: 2024/12/14 15:22:12 by amaatall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t n, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*dst;
 
-	if (n == 0 || size == 0)
-		return (malloc(0));
-	if (size && SIZE_MAX / size < n)
+	if (size && SIZE_MAX / size < nmemb)
 		return (NULL);
-	dst = malloc(n * size);
+	dst = (void *)malloc(nmemb * size + 1);
 	if (dst == NULL)
 		return (NULL);
-	ft_memset(dst, 0, size * n);
+	ft_bzero(dst, size * nmemb);
 	return (dst);
 }
